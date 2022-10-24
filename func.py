f8bb5c61bc1c7cdeabc31442336e5cf31a50dad4
Maks Golovin <maxxig@gmail.com>
2022-10-24 11:58:12 +0300

    remove topic size

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def sendEmail(text, login, password, emails_to, logger, priority = '4'):
+def sendEmail(text, login, password, emails_to, priority = '4'):
     msg = EmailMessage()
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
     smtpObj.starttls()
@@ -16,26 +16,30 @@ def sendEmail(text, login, password, emails_to, logger, priority = '4'):
     msg['To'] = emails_to
     smtpObj.send_message(msg)
     smtpObj.close()
-    logger.info(f'Email sended')
 def get_deadletters_cnt(servicebus_mgmt_client):
     result = {}
     for topic_properties in servicebus_mgmt_client.list_topics():
         subs = servicebus_mgmt_client.list_subscriptions(topic_properties.name)
         for sub in subs:
             prop = servicebus_mgmt_client.get_subscription_runtime_properties(topic_properties.name, sub.name)
-            if (prop.dead_letter_message_count > 0):
+            if (prop.dead_letter_message_count > 0): # or topic_properties.size_in_bytes > 314572800):
                 # print("Topic Name: " + topic_properties.name +", " + sub.name + ": total_message_count - " + str(prop.total_message_count) +
                 #     ", active_message_count:" + str(prop.active_message_count) +
                 #     ", dead_letter_message_count:" + str(prop.dead_letter_message_count)
                 #     )
-                result[topic_properties.name] = {'Subscription': sub.name, 'dead_letters_count': prop.dead_letter_message_count}
+                result[topic_properties.name] = {
+                    #'Size': topic_properties.size_in_bytes/1024/1024,
+                    'Subscription': sub.name
+                    ,'dead_letters_count': prop.dead_letter_message_count}
     return result
+
 def generate_html_table(data):
     html = '<table border="1"><tr><th>Topic</th><th>Subscription</th><th>DeadLetters</th></tr>'
     for k, value in data.items():
         html += f'<tr><td>{str(k)}</td><td>{value["Subscription"]}</td><td>{value["dead_letters_count"]}</td></tr>'
     html += '</table>'
     return html
+
 def check_previous_version(new_data):
     filename = 'old_deadletters_list.json'
     old_data = {}
--- service_bus_monitoring.py
+++ service_bus_monitoring.py
@@ -12,21 +12,20 @@ from azure.servicebus.management import ServiceBusAdministrationClient
 from azure.servicebus import ServiceBusClient, TransportType
 
 CONNECTION_STR = cnfg['service_bus_connection_string']
-while(1):
-    dt = datetime.now()
-    logger.info(f'Start processing {dt}')
-    if dt.weekday() < 5 and dt.hour > 8 and dt.hour < 18:
-        # is for test
-        #result_dict = {'es-bulk': {'Subscription': 'product.complete', 'dead_letters_count': 221}, 'products-prices-publishing': {'Subscription': 'exec', 'dead_letters_count': 14}, 'products-publishing': {'Subscription': 'oneday', 'dead_letters_count': 35}, 'products-update': {'Subscription': 'stock-management', 'dead_letters_count': 11833}, 'sm-stock-update': {'Subscription': 'elasticsearch', 'dead_letters_count': 1943}, 'variation-groups-cache-update': {'Subscription': 'change-variation-groups-events-to-markethub', 'dead_letters_count': 158}}
-        result_dict = {}
-        with ServiceBusAdministrationClient.from_connection_string(CONNECTION_STR) as servicebus_mgmt_client:
-            result_dict = func.get_deadletters_cnt(servicebus_mgmt_client)
 
-        #with ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=False, transport_type = TransportType.AmqpOverWebsocket) as servicebus_client:
-        logger.info(f'Deadletters info: {str(result_dict)}')
-        is_new_value = func.check_previous_version(result_dict)
-        logger.info(f'Is new value: {is_new_value}')
-        if len(result_dict) > 0 and is_new_value:
-            func.sendEmail(func.generate_html_table(result_dict), cnfg['email_login'], cnfg['email_password'], cnfg['emails_to'], logger, '1')
-    logger.info(f'Stop processing: {datetime.now()}. Waiting...')
-    time.sleep(10800)
+dt = datetime.now()
+logger.info(f'{dt}: Start processing.')
+# is for test
+#result_dict = {'es-bulk': {'Subscription': 'product.complete', 'dead_letters_count': 221}, 'products-prices-publishing': {'Subscription': 'exec', 'dead_letters_count': 14}, 'products-publishing': {'Subscription': 'oneday', 'dead_letters_count': 35}, 'products-update': {'Subscription': 'stock-management', 'dead_letters_count': 11833}, 'sm-stock-update': {'Subscription': 'elasticsearch', 'dead_letters_count': 1943}, 'variation-groups-cache-update': {'Subscription': 'change-variation-groups-events-to-markethub', 'dead_letters_count': 158}}
+result_dict = {}
+with ServiceBusAdministrationClient.from_connection_string(CONNECTION_STR) as servicebus_mgmt_client:
+    result_dict = func.get_deadletters_cnt(servicebus_mgmt_client)
+
+#with ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=False, transport_type = TransportType.AmqpOverWebsocket) as servicebus_client:
+logger.info(f'{datetime.now()}: Deadletters info: {str(result_dict)}')
+is_new_value = func.check_previous_version(result_dict)
+logger.info(f'{datetime.now()}: Is new value: {is_new_value}')
+if len(result_dict) > 0 and is_new_value:
+    func.sendEmail(func.generate_html_table(result_dict), cnfg['email_login'], cnfg['email_password'], cnfg['emails_to'], '1')
+    logger.info(f'{datetime.now()}: Email sended')
+logger.info(f'{datetime.now()}: Stop processing.')
